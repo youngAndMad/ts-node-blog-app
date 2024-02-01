fa8e947f625448310b0bf391fcc31a58f61ae7da
youngandmad <kkraken2005@gmail.com>
2024-02-02 03:18:45 +0600

    fixed minio config

--- src/config/minio.config.ts
+++ src/config/minio.config.ts
@@ -1,11 +1,12 @@
 import { Client } from "minio";
+import { ENV } from "./env.config";
 
 const minioClient = new Client({
-  endPoint: "minio-server",
-  port: 9000,
-  useSSL: false,
-  accessKey: "your-access-key",
-  secretKey: "your-secret-key",
+  endPoint: ENV.MINIO_ENDPOINT,
+  port: ENV.MINIO_PORT,
+  useSSL: ENV.MINIO_USE_SSL,
+  accessKey: ENV.MINIO_ACCESS_KEY,
+  secretKey: ENV.MINIO_SECRET_KEY,
 });
 
 export default minioClient;
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import logger from "./provider/logger";
 import cors from "cors";
 import { ENV } from "./config/env.config";
 import prisma from "./config/prisma.config";
+import fileUpload from "express-fileupload";
 import userRouter from "./router/user.router";
 import requestLoggerMiddleware from "./middleware/logging.middleware";
 
 const app: Express = express();
 app.use(json());
+app.use(fileUpload());
 app.use(cors());
 app.use(requestLoggerMiddleware);
 const port = ENV.PORT;
