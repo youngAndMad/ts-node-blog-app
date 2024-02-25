44164187655e568eb1c981e2b9e1ccf3509bb933
youngandmad <kkraken2005@gmail.com>
2024-02-25 15:54:56 +0600

    pass * instead of array

--- src/provider/socket.ts
+++ src/provider/socket.ts
@@ -19,9 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["*"],
-    methods: ["*"],
-    allowedHeaders: ["*"],
+    origin: "*",
   },
 });
 
