c621163e11a77cc77c1a1d98d1c833cabb732b17
youngandmad <kkraken2005@gmail.com>
2024-02-25 15:46:13 +0600

    fixed cors for socket io

--- src/provider/socket.ts
+++ src/provider/socket.ts
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:4200"],
+    origin: ["*"],
     methods: ["GET", "POST", "DELETE"],
   },
 });
